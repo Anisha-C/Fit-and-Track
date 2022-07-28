d1adcbedaef229569eb12e226d0aa72536a5af2a
Anisha-C <99618409+Anisha-C@users.noreply.github.com>
2022-07-27 20:45:24 -0400

    Added to Exercse and app.js

--- fit-and-track/src/App.js
+++ fit-and-track/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import { useState } from 'react';
+import Exercise from "./components/pages/Exercise";
+import Food from "./components/pages/Food";
+import Water from "./components/pages/Water";
+import User from "./components/pages/User";
+import Contact from './components/pages/Contact';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('Exercise')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar changePage={setCurrentPage} />
+      <Header title="Fit and Track" subtitle="Welcome to the Fit and Track App" />
+      {currentPage === "Excercise" ? <Exercise />
+        : currentPage === "Food" ? <Food />
+          : currentPage === "Water" ? <Water />
+            : currentPage === "User" ? <User />
+              : <Contact />}
     </div>
   );
 }
--- fit-and-track/src/components/pages/Exercise.js
+++ fit-and-track/src/components/pages/Exercise.js
@@ -1,11 +1,17 @@
 function Exercise(){
+    const exercise = {
+        username: this.state.username,
+        description: this.state.description,
+        duration: this.state.duration,
+        date: this.state.date,
+      };
 
-    return (
+   /* return (
         <div id="Exercise" class="intro">
             <h1 class ="">Exercise</h1>
             <p></p>
         </div>
 
-    )
+    )*/
 }
 export default Exercise
