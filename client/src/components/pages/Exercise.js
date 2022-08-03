40c71b9ae7a9365fcbe096361c763a0f0bba79c3
Anisha-C <99618409+Anisha-C@users.noreply.github.com>
2022-08-03 19:56:11 -0400

    added exercise log

--- client/src/components/pages/Exercise.js
+++ client/src/components/pages/Exercise.js
@@ -2,74 +2,65 @@ import { useState, useEffect } from "react";
 import Exerciselog from "../Exerciselog";
 import '../../semantic/dist/components/grid.css';
 import '../../semantic/dist/components/list.css';
-//dummy data for viewing while backend is being set up
-const tempdata = [
-    {
-        description: "Cardio",
-        duration: "30",
-        date:""
-    },
-    {
-        description: "Bench Press",
-        duration: "15",
-        date: ""
-    },
-    {
-        description: "Legs",
-        duration: "45",
-        date: ""
-    },
-]
-// const [newExercise, setnewExercise] = useState({
-//     description: "",
-//     duration: "",
-//     date: "",
-// })
 
-// function saveExercise() {
-//     fetch("http://localhost:3001/exercise/add", {
-//         method: 'POST',
-//         headers: {
-//             Accept: 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(newExercise)
-//     }).then((res) => {
-//         console.log(res)
-//         setnewExercise({
-//             description: "",
-//             duration: "",
-//             date: "",
-//         })
-//     }).catch(err => {
-//         console.log(err)
-//     })}
-
-// function handleexerciseInput(e) {
-//     const { name, value } = e.target
-//     setnewExercise(prev => ({ ...prev, [name]: value }))
-// }
 
 function Exercise(props) {
-    const [savedExercises, setsavedExercises] = useState(tempdata)
+    const [savedExercises, setsavedExercises] = useState(null)
+    const [newExercise, setnewExercise] = useState({
+        description: "",
+        duration: "",
+        date: "",
+    })
+    // useEffect(() => {
+    //     fetch("/exercise").then(res => {
+    //         setsavedExercises(res)
+    //     })
+    // }, [])
+    useEffect(() => {
+        getExercise();
+
+    }, [])
 
     useEffect(() => {
-        fetch("/exercise").then(res => {
-            setsavedExercises(res)
-        })
-    }, [])
-    //Delete line above and uncomment bottom once backend has exercise route
-    // const [savedExercises, setsavedExercises] = useState(getExercises())
-    async function getExercises() {
+        console.log(savedExercises)
+
+    }, [savedExercises])
+
+    function handleexerciseInput(e) {
+        const { name, value } = e.target
+        setnewExercise(prev => ({ ...prev, [name]: value }))
+    }
+    
+    async function getExercise() {
         const exercises = await fetch("/exercise")
         console.log(exercises)
         return exercises
     }
+    function saveExercise() {
+    
+        fetch("http://localhost:3001/exercise/add", {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newExercise)
+        }).then((res) => {
+            console.log(res)
+            setnewExercise({
+                description: "",
+                duration: "",
+                date: "",
+            })
+        }).catch(err => {
+            console.log(err)
+        })}
+    
     return (
         <div class="ui two column centered grid">
             <div>
                 <h1>Exercise Log</h1>
-                {/* <div>
+                <div>
                 <label for="description" >Description</label>
                 <input id="description" type="text" onChange={handleexerciseInput} value={newExercise.description} name="description" />
                 <label for="duration" >Duration</label>
@@ -77,10 +68,10 @@ function Exercise(props) {
                 <label for="date" >Date</label>
                 <input id="date" type="text" onChange={handleexerciseInput} value={newExercise.date} name="date" />
                 <button onClick={saveExercise}>Submit</button>
-            </div> */}
+            </div>
                 <div className="ui list">
                     <div class="item">
-                        {savedExercises.length && savedExercises.map((ex, i) => <Exerciselog key={ex.description + i} description={ex.description} duration={ex.duration} date={ex.date} />)}
+                        {savedExercises && savedExercises.map((ex, i) => <Exerciselog key={ex.description + i} description={ex.description} duration={ex.duration} date={ex.date} />)}
                     </div>
                 </div>
             </div>
