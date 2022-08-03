7de7e20ff0fdba80a1e6d52942698ac30c7d3ef5
Anisha-C <99618409+Anisha-C@users.noreply.github.com>
2022-08-03 19:33:15 -0400

    Trying to fix server connection issue

--- client/src/components/pages/Exercise.js
+++ client/src/components/pages/Exercise.js
@@ -20,6 +20,36 @@ const tempdata = [
         date: ""
     },
 ]
+// const [newExercise, setnewExercise] = useState({
+//     description: "",
+//     duration: "",
+//     date: "",
+// })
+
+// function saveExercise() {
+//     fetch("http://localhost:3001/exercise/add", {
+//         method: 'POST',
+//         headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(newExercise)
+//     }).then((res) => {
+//         console.log(res)
+//         setnewExercise({
+//             description: "",
+//             duration: "",
+//             date: "",
+//         })
+//     }).catch(err => {
+//         console.log(err)
+//     })}
+
+// function handleexerciseInput(e) {
+//     const { name, value } = e.target
+//     setnewExercise(prev => ({ ...prev, [name]: value }))
+// }
+
 function Exercise(props) {
     const [savedExercises, setsavedExercises] = useState(tempdata)
 
@@ -31,7 +61,7 @@ function Exercise(props) {
     //Delete line above and uncomment bottom once backend has exercise route
     // const [savedExercises, setsavedExercises] = useState(getExercises())
     async function getExercises() {
-        const exercises = await fetch("/api/user/exercise")
+        const exercises = await fetch("/exercise")
         console.log(exercises)
         return exercises
     }
@@ -39,6 +69,15 @@ function Exercise(props) {
         <div class="ui two column centered grid">
             <div>
                 <h1>Exercise Log</h1>
+                {/* <div>
+                <label for="description" >Description</label>
+                <input id="description" type="text" onChange={handleexerciseInput} value={newExercise.description} name="description" />
+                <label for="duration" >Duration</label>
+                <input id="duration" type="text" onChange={handleexerciseInput} value={newExercise.duration} name="duration" />
+                <label for="date" >Date</label>
+                <input id="date" type="text" onChange={handleexerciseInput} value={newExercise.date} name="date" />
+                <button onClick={saveExercise}>Submit</button>
+            </div> */}
                 <div className="ui list">
                     <div class="item">
                         {savedExercises.length && savedExercises.map((ex, i) => <Exerciselog key={ex.description + i} description={ex.description} duration={ex.duration} date={ex.date} />)}
